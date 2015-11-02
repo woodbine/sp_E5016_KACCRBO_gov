a20793d7a9fd5ff0c0f2aa50f49aee8c29eb3e0c
blablupcom <olegvolodin@eyahoo.com>
2015-11-02 22:58:59 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 #### IMPORTS 1.0
@@ -9,7 +8,6 @@ import scraperwiki
 import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
-import requests
 
 #### FUNCTIONS 1.0
 
@@ -39,26 +37,24 @@ def validateFilename(filename):
 
 def validateURL(url):
     try:
-        r = requests.get(url, allow_redirects=True, timeout=20)
+        r = urllib2.urlopen(url)
         count = 1
-        while r.status_code == 500 and count < 4:
+        while r.getcode() == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = requests.get(url, allow_redirects=True, timeout=20)
+            r = urllib2.urlopen(url)
         sourceFilename = r.headers.get('Content-Disposition')
-
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
-        if r.headers['Content-Type'] == 'application/octet-stream':
-            ext = '.csv'
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.status_code == 200
-        validFiletype = ext in ['.csv', '.xls', '.xlsx', '.CSV']
+        validURL = r.getcode() == 200
+        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
     except:
         print ("Error validating URL.")
         return False, False
+
 
 def validate(filename, file_url):
     validFilename = validateFilename(filename)
@@ -117,7 +113,7 @@ for link in links:
         elif 'Quarter three' in csvfiles:
             csvMth = 'Q3'
         elif 'Quarter four' in csvfiles:
-            csvMth = 'Q3'
+            csvMth = 'Q4'
         if ' to ' or '-' in csvfiles:
             filequater = csvfiles.split(' ')[-2]
         if 'March' in filequater:
@@ -126,7 +122,6 @@ for link in links:
             csvMth = 'Q0'
         elif 'July' in filequater:
             csvMth = 'Q0'
-
         csvMth = convert_mth_strings(csvMth.upper())
         data.append([csvYr, csvMth, url])
 
